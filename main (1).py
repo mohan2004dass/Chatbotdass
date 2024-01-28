29b0fc3de544809401e002da49dcc560420ecc3e
mohan2004dass <108020831+mohan2004dass@users.noreply.github.com>
2024-01-28 19:26:33 +0530

    Add files via upload

---
+++ main (1).py
@@ -0,0 +1,11 @@
+import streamlit as st
+
+st.title("chat bot")
+
+command = st.chat_input("how can i help you")
+
+if command:
+    with st.chat_message("USER"):
+        st.write(command)
+    if "hello" in command:
+        with st.chat_message("bot"):
